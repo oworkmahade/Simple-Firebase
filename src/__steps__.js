bb1573011f4fdf1b2f12fe66debb1d11b5f141c4
mahade <owork.hasan@gmail.com>
2026-03-02 22:16:05 +0600

    commited

--- src/__steps__.js
+++ src/__steps__.js
@@ -3,8 +3,10 @@
  * 2.console.firebase.google.com create project
  * 3.register project with web
  * 4.get firebase config and do not save config on the client side
- * 5.export app from firebase.init.js
- * 6.create auth where it will be added
+ * 5.create auth where it will be added
+ * 6.export auth from firebase.init.js
+ * 7.in the console authentication > started > enable Google Provider
+ * 8.
  *
  *
  */
--- src/components/Layouts/Login/Login.jsx
+++ src/components/Layouts/Login/Login.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 import { Helmet } from "react-helmet-async";
+import auth from "../../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
+  console.log(user);
+
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    // console.log(auth, provider);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // console.log(result);
+        // console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+        setUser(null);
+      });
+  };
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("sign out done");
+        setUser(null);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
       <Helmet>
         {" "}
         <title>Login | Firebase</title>
       </Helmet>
-      <button>Login with Google</button>
+      <button onClick={handleGoogleSignIn}>Login with Google</button>
+      <br></br>
+      <button onClick={handleGoogleSignOut}>Sign Out</button>
+
+      {user && (
+        <div>
+          <h2>Name: {user.displayName}</h2>
+          <h2>Email: {user.email}</h2>
+          <img src={user.photoURL} alt="User images" />
+        </div>
+      )}
     </div>
   );
 };
